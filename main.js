19b4d9427bb88378662f4a08d04f588bf87f9b96
Clads <adriano.clads@gmail.com>
2023-05-19 08:20:42 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form-agenda');
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefone = document.getElementById('telefone');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+});
