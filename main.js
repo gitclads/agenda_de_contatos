41e540b5e2c1750cb6e1397194c8f911843afa35
Clads <adriano.clads@gmail.com>
2023-05-25 21:32:24 -0300

    commit 25/05/2023

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 const form = document.getElementById('form-agenda');
+const nome = [];
+const telefone = [];
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
     const inputNomeContato = document.getElementById('nome-contato');
     const inputTelefone = document.getElementById('telefone');
 
-    let linha = '<tr>';
-    linha += `<td>${inputNomeContato.value}</td>`;
-    linha += `<td>${inputTelefone.value}</td>`;
-    linha += '</tr>';
+    if (telefone.includes(inputTelefone.value)) {
+        alert(`Este telefone ${inputTelefone.value} já existe`);
+    } else {
+        nome.push(inputNomeContato.value);
+        telefone.push(inputTelefone.value);
+    
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += '</tr>';
+    
+        linhas = linha;
+    }
 
-    linhas += linha;
+    inputNomeContato.value = '';
+    inputTelefone.value = '';
+}
 
+function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-});
+    corpoTabela.innerHTML += linhas;
+}
+
+
